597243d6ce566538a6cd330ec1d1165bb3d58ed0
pasquale <pasfranc@gmail.com>
2021-04-13 15:08:07 +0200

    Changes to copy all needed files at npm run build

--- lib/fileService.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const fileExists: (filePath: string) => boolean;
-export declare const readJSONFromFile: (filePath: string) => any;
-export declare const readStringFromFile: (filePath: string) => string;
