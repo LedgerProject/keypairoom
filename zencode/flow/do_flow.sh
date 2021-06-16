f008ec0d60cc92f2aa3c7369e441c968012dc117
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2021-06-16 17:46:05 +0200

    Checkin last version

--- zencode/flow/do_flow.sh
+++ zencode/flow/do_flow.sh
@@ -10,8 +10,17 @@ recreated_keypair=recreated_keypair.keys
 # Hash questions and answers to send to the server to check these are the correct ones
 ${zenroom} -a client_start.data -z client_start.zen | jq '.' > ${client_input}
 
-# Compare hashes and if successful give key
-${zenroom} -a ${client_input} -k server_reply.keys -z server_reply.zen | jq '.' > ${key_derivation}
+# Compare hashes
+result=$(${zenroom} -a ${client_input} -k server_verify.keys -z server_verify.zen | jq '.output[0]')
+
+if [ ! "${result} " == '"Questions_and_Answers_match" ' ]
+then
+    echo -e "Result is ${result}"
+    exit -1
+fi
+
+# give key
+${zenroom} -a ${client_input} -k server_gen.keys -z server_gen.zen | jq '.' > ${key_derivation}
 
 # Regenerate keypair
 ${zenroom} -a ${client_input} -k ${key_derivation} -z client_make_keypair.zen | jq '.' > ${recreated_keypair}
